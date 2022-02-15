6b826c5febd011e0ecf7fff11055416557a77536
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-02-15 09:34:00 +0900

    再レンダリングを起こす準備

--- ChildArea.jsx
+++ ChildArea.jsx
@@ -1,11 +1,17 @@
 const style = {
-  width: "100px",
+  width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
 export const ChildArea = (props) => {
   const { open } = props;
+  console.log("childareaがレンダリングされた");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
   return (
     <>
       {open ? (

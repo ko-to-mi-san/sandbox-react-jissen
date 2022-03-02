e03532bcb9b4cdbb2f753cb2fa3de5b62354ef91
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-03-02 09:34:47 +0900

    organisms設定

---
+++ src/components/Organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>foo@example.com</dd>
+        <dt>TEL</dt>
+        <dd>0120-999-999</dd>
+        <dt>会社名</dt>
+        <dd>fooさん株式会社</dd>
+        <dt>WEB</dt>
+        <dd>http://foo.example.com</dd>
+      </dl>
+    </div>
+  );
+};

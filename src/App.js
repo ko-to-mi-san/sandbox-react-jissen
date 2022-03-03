06f4a5a6863423eaa8581f8ef3c66e4e845593c1
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-03-03 16:43:04 +0900

    usercard作成途中

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/Organisms/user/UserCard";
 import "./styles.css";
 
+const user = {
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  name: "ぴーつん",
+  email: "12345@example.com",
+  phone: "0120-123-456",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
+
 export default function App() {
   return (
     <div className="App">
@@ -11,7 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
--- src/components/Organisms/user/UserCard.jsx
+++ src/components/Organisms/user/UserCard.jsx
@@ -1,18 +1,33 @@
-export const UserCard = () => {
+import styled from "styled-components";
+
+export const UserCard = (props) => {
+  const { user } = props;
   return (
     <div>
-      <img alt="プロフィール" />
-      <p>名前</p>
-      <dl>
+      <img heigth={160} width={160} src={user.image} alt={user.name} />
+      <p>{user.name}</p>
+      <SDl>
         <dt>メール</dt>
-        <dd>foo@example.com</dd>
+        <dd>{user.email}</dd>
         <dt>TEL</dt>
-        <dd>0120-999-999</dd>
+        <dd>{user.phone}</dd>
         <dt>会社名</dt>
-        <dd>fooさん株式会社</dd>
+        <dd>{user.company.name}</dd>
         <dt>WEB</dt>
-        <dd>http://foo.example.com</dd>
-      </dl>
+        <dd>{user.website}</dd>
+      </SDl>
     </div>
   );
 };
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+  }
+`;

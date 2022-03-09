a2deabb54b4ed73816eedd95ee6ceea9af0508ec
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-03-09 10:07:57 +0900

    pageの作成

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atom/button/PrimaryButton";
-import { SecondaryButton } from "./components/atom/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/Organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  name: "ぴーつん",
-  email: "12345@example.com",
-  phone: "0120-123-456",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }

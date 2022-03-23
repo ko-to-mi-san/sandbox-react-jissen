923fb7e569811d20944c2e08cedc745cc56afa39
kotomi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-03-23 09:44:05 +0900

    jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import axios from "axios";
 import { RecoilRoot } from "recoil";
 import { UserProvider } from "./providers/UserProvider";
 import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <RecoilRoot>
       <UserProvider>
         <Router />
+        <button onClick={onClickUsers}>Users</button>
+        <button onClick={onClickUser1}>id=1のuser</button>
       </UserProvider>
     </RecoilRoot>
   );
